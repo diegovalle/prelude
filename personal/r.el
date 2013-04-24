874614b101c483637712ebcf88cf348569d017ac
Diego Valle-Jones <diegovalle@gmail.com>
2013-04-23 20:35:55 -0500

    my emacs config

---
+++ personal/r.el
@@ -0,0 +1,74 @@
+;;; R.el --- Configure R just the way I like it
+;;
+;; Copyright (c) 2013 Diego Valle Jones
+;;
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This file simply sets up R
+;; Use shift-enter to split window & launch R (if not running), execute highlighted
+;; region (if R running & area highlighted), or execute current line
+;; (and move to next line, skipping comments). Nice.
+;; See http://www.emacswiki.org/emacs/EmacsSpeaksStatistics,
+;; FelipeCsaszar. Adapted to spilit vertically instead of
+;; horizontally.
+
+;;; Code:
+
+;; Use shift-enter to split window & launch R (if not running), execute highlighted
+;; region (if R running & area highlighted), or execute current line
+;; (and move to next line, skipping comments). Nice.
+;; See http://www.emacswiki.org/emacs/EmacsSpeaksStatistics,
+;; FelipeCsaszar. Adapted to spilit vertically instead of
+;; horizontally.
+;; Adapted with one minor change from Felipe Salazar at
+;; http://www.emacswiki.org/emacs/EmacsSpeaksStatistics
+
+(setq ess-ask-for-ess-directory nil)
+(setq ess-local-process-name "R")
+(setq ansi-color-for-comint-mode 'filter)
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
+(defun my-ess-start-R ()
+  (interactive)
+  (if (not (member "*R*" (mapcar (function buffer-name) (buffer-list))))
+      (progn
+        (delete-other-windows)
+        (setq w1 (selected-window))
+        (setq w1name (buffer-name))
+        (setq w2 (split-window w1 nil t))
+        (R)
+        (set-window-buffer w2 "*R*")
+        (set-window-buffer w1 w1name))))
+(defun my-ess-eval ()
+  (interactive)
+  (my-ess-start-R)
+  (if (and transient-mark-mode mark-active)
+      (call-interactively 'ess-eval-region)
+    (call-interactively 'ess-eval-line-and-step)))
+(add-hook 'ess-mode-hook
+          '(lambda()
+             (local-set-key [(ctrl return)] 'my-ess-eval)))
+(add-hook 'inferior-ess-mode-hook
+          '(lambda()
+             (local-set-key [C-up] 'comint-previous-input)
+             (local-set-key [C-down] 'comint-next-input)))
+(add-hook 'Rnw-mode-hook
+          '(lambda()
+             (local-set-key [(shift return)] 'my-ess-eval)))
+(require 'ess-site)
+
+
+
+;;describe data.frames in R
+(require 'ess-R-object-popup)
+(define-key ess-mode-map "\C-c\C-g" 'ess-R-object-popup)
+
+;;don't prompt on exit
+(setq inferior-ess-exit-command "q('no')")
+'(inferior-R-args "--no-save --no-restore-data --quiet")
+
+;;; R.el ends here
---
+++ personal/utils.el
@@ -0,0 +1,74 @@
+;;cool lambda character
+(require 'lambda-mode)
+(add-hook 'python-mode-hook #'lambda-mode 1)
+(setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
+
+;;(elscreen-start)
+(global-set-key "\C-x\C-b" 'ibuffer)
+
+;; resize window on start
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 160))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist 
+         (cons 'height (/ (- (x-display-pixel-height) 200)
+                             (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
+
+;; emacs droppings in their own directory
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+
+;;autocomplete only works on R files if I add this????
+(setq ess-use-auto-complete 'script-only)
+
+;; Auto complete
+(add-to-list 'load-path "~/.emacs.d/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict")
+(ac-config-default)
+
+
+;; line numbers
+(global-linum-mode 1)
+(setq linum-format "%3d");" \u2502 ")
+
+
+;; nice fonts
+(defun font-existsp (font)
+  "Check to see if the named FONT is available."
+  (if (null (x-list-fonts font))
+      nil t))
+
+(cond
+ ((eq window-system nil) nil)
+ ((font-existsp "PragmataPro")
+  (set-face-attribute 'default nil :height 121 :font "PragmataPro"))
+ ((font-existsp "Menlo")
+  (set-face-attribute 'default nil :height 121 :font "Meslo"))
+ ((font-existsp "Consolas")
+  (set-face-attribute 'default nil :height 121 :font "Consolas"))
+ ((font-existsp "Inconsolata")
+  (set-face-attribute 'default nil :height 121 :font "Inconsolata"))
+ )
+
+;; whitespace-mode
+;; free of trailing whitespace and to use 80-column width, standard indentation
+;;(setq whitespace-style '(trailing lines space-before-tab
+;;                                  indentation space-after-tab)
+;;      whitespace-line-column 80)
