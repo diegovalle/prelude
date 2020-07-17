f38c7b3c58f30100a5d229c2809491e86c2454ec
diego <git@diegovalle.net>
2020-07-16 22:17:52 -0500

    smooth scrolling and monokai theme

--- personal/init-personal.el
+++ personal/init-personal.el
@@ -34,7 +34,7 @@
 
 
 (package-initialize)
-(load-theme 'darkplus)
+(load-theme 'monokai-pro t)
 
 (require 'linum-highlight-current-line-number)
 
@@ -74,3 +74,43 @@
 (customize-set-variable 'org-journal-dir "~/Documents/org/journal/")
 (customize-set-variable 'org-journal-date-format "%A, %B %d, %Y")
 (require 'org-journal)
+
+;; from http://stackoverflow.com/questions/3633120/emacs-hotkey-to-align-equal-signs
+;; another information: https://gist.github.com/700416
+;; use rx function http://www.emacswiki.org/emacs/rx
+
+
+(defun align-to-colon (begin end)
+  "Align region to colon (:) signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx (group (zero-or-more (syntax whitespace))) ":") 1 1 ))
+
+(defun align-to-comma (begin end)
+  "Align region to comma  signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx "," (group (zero-or-more (syntax whitespace))) ) 1 1 ))
+
+
+(defun align-to-equals (begin end)
+  "Align region to equal signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx (group (zero-or-more (syntax whitespace))) "=") 1 1 ))
+
+(defun align-to-hash (begin end)
+  "Align region to hash ( => ) signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx (group (zero-or-more (syntax whitespace))) "=>") 1 1 ))
+
+;; work with this
+(defun align-to-comma-before (begin end)
+  "Align region to equal signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx (group (zero-or-more (syntax whitespace))) ",") 1 1 ))
+
+(require 'smooth-scroll)
+(smooth-scroll-mode t)
